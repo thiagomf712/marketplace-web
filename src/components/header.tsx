e357e6580d85c915c8cc46c23c17287472e1f477
Thiago Ferreira <thiagomf712@gmail.com>
2024-08-24 09:50:12 -0300

    limit header max width content

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,28 +13,30 @@ import { NavLink } from './nav-link'
 
 export function Header() {
   return (
-    <header className="mb-16 flex h-20 items-center justify-between border-b border-b-shape px-5 py-4">
-      <img src={logo} alt="" className="my-1 h-10 w-14" />
+    <header className="mb-16 h-20 border-b border-b-shape px-5 py-4">
+      <div className="mx-auto flex max-w-screen-2xl items-center justify-between">
+        <img src={logo} alt="" className="my-1 h-10 w-14" />
 
-      <nav className="my-1 flex items-center gap-2">
-        <NavLink to="/" Icon={ChartHistogramIcon}>
-          Dashboard
-        </NavLink>
+        <nav className="my-1 flex items-center gap-2">
+          <NavLink to="/" Icon={ChartHistogramIcon}>
+            Dashboard
+          </NavLink>
 
-        <NavLink to="/products" Icon={PackageIcon}>
-          Produtos
-        </NavLink>
-      </nav>
+          <NavLink to="/products" Icon={PackageIcon}>
+            Produtos
+          </NavLink>
+        </nav>
 
-      <div className="flex items-center gap-4">
-        <Button size="sm" asChild>
-          <Link to="/new-product">
-            <PlusSignIcon />
-            Novo produto
-          </Link>
-        </Button>
+        <div className="flex items-center gap-4">
+          <Button size="sm" asChild>
+            <Link to="/new-product">
+              <PlusSignIcon />
+              Novo produto
+            </Link>
+          </Button>
 
-        <AccountMenu />
+          <AccountMenu />
+        </div>
       </div>
     </header>
   )
